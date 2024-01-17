8abf6e34da84ac4e1125f75db92150a525c31450
Haider <haidershanna@gmail.com>
2024-01-18 01:37:54 +0300

    Made the nav header responsive

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const navHeader = document.getElementById('navHeader');
+const hamMenu = document.getElementById('hamMenu');
+const closeMenuVar = document.getElementById('closeMenu');
 
 addEventListener('scroll', ()=>{
   let scrolled = scrollY;
@@ -9,3 +11,21 @@ addEventListener('scroll', ()=>{
     navHeader.classList.remove('scrolled');
   }
 })
+
+if(navHeader.className === "nav-header"){
+  closeMenuVar.style.display = "none";
+}
+else{
+  hamMenu.style.display = "none";
+}
+
+function showMenu(){
+  navHeader.className = "clicked";
+  hamMenu.style.display = "none";
+  closeMenuVar.style.display = "block";
+}
+function closeMenu(){
+  navHeader.className = "nav-header";
+  hamMenu.style.display = "block";
+  closeMenuVar.style.display = "none";
+}
